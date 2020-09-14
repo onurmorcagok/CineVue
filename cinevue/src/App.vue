129834f10dcd37368a1f82a9c29fe4cc3dfc585e
Onur MORCAGÖK <55251990+onurmorcagok@users.noreply.github.com>
2020-09-14 17:25:17 +0300

    Project updated

--- cinevue/src/App.vue
+++ cinevue/src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
-
+    <appNavbar></appNavbar>
   </div>
+
 </template>
 
 <script>
+import Navbar from './components/Navbar'
   export default {
-    
+    name: 'App',
+    components: {
+      appNavbar: Navbar,
+    }
   }
 </script>
 
---
+++ cinevue/src/components/Navbar.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-warning">
+      <a class="navbar-brand" href="#">
+        <img src="https://pbs.twimg.com/profile_images/1288534032020647936/FR_a2W9i_400x400.jpg" style="width: 100px;" />
+        CineVue
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">
+              Home
+              <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Disabled</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+
+<style>
+</style>
