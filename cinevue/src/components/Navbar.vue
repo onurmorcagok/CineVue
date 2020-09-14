c13021486eeadf9ad8a52fb27b024a77df49bedc
Onur MORCAGÖK <55251990+onurmorcagok@users.noreply.github.com>
2020-09-14 20:37:06 +0300

    Project updated

--- cinevue/src/components/Navbar.vue
+++ cinevue/src/components/Navbar.vue
@@ -2,8 +2,12 @@
   <div>
     <nav class="navbar navbar-expand-lg navbar-light bg-warning">
       <a class="navbar-brand" href="#">
-        <img src="https://pbs.twimg.com/profile_images/1288534032020647936/FR_a2W9i_400x400.jpg" style="width: 100px;" />
-        CineVue
+        <img
+          class="img"
+          src="https://pbs.twimg.com/profile_images/1288534032020647936/FR_a2W9i_400x400.jpg"
+          style="width: 70px;"
+        />
+        <label class="label">CineVue</label>
       </a>
       <button
         class="navbar-toggler"
@@ -18,20 +22,36 @@
       </button>
 
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
+        <ul class="navbar-nav mx-auto">
           <li class="nav-item active">
             <a class="nav-link" href="#">
-              Home
+              <i class="fas fa-thumbs-up">
+                  <span>Popular</span></i>
               <span class="sr-only">(current)</span>
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
+            <a class="nav-link" href="#">
+              <i class="fas fa-play-circle">
+                  <span>Now Playing</span></i>
+            </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Disabled</a>
+            <a class="nav-link" href="#">
+              <i class="fas fa-list-alt">
+                  <span>Top Rated</span></i>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <i class="fas fa-file-video">
+                  <span>Upcoming</span></i>
+            </a>
           </li>
         </ul>
+        <div class="icon">
+          <button class="btn btn-dark">SIGN IN</button>
+        </div>
       </div>
     </nav>
   </div>
@@ -44,4 +64,25 @@ export default {
 </script>
 
 <style>
+.img {
+  border-radius: 50%;
+}
+
+.label {
+  margin-left: 10px;
+  font-family: "Racing Sans One", cursive;
+  font-size: 30px;
+}
+
+.icon {
+  margin-right: 80px;
+}
+
+span {
+    margin-left:3px;
+}
+
+li {
+    font-size: 20px;
+}
 </style>
