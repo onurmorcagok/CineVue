9687d8791b61fb75e1412e092ad23d9ca76a1b6c
Onur MORCAGÖK <55251990+onurmorcagok@users.noreply.github.com>
2020-09-18 21:37:46 +0300

    Update Search.vue

--- cinevue/src/components/Search.vue
+++ cinevue/src/components/Search.vue
@@ -1,33 +1,33 @@
 <template>
-    <div class="search">
-        <input
-        type="text"
-        class="search-box search-bar form-control p-4 my-4"
-        placeholder="Search by movie title"
-        v-model="searchMovie">
-    </div>
+  <div class="search">
+    <input
+      type="text"
+      class="search-box search-bar form-control p-4 my-4"
+      placeholder="Search by movie title"
+      v-model="searchMovie"
+    />
+  </div>
 </template>
 
 <script>
 import debounce from "lodash/debounce";
-    export default {
-        name: 'Search',
-        data() {
-            return {
-                searchMovie: '',
-            }
-        },
-        watch: {
-            searchMovie: debounce(function(newVal){
-                this.$store.dispatch("movies/search_movie", newVal);
-                this.$router.push({name: 'Search', params: { name: newVal }});
-            },500)
-        }
-    }
+export default {
+  name: "Search",
+  data() {
+    return {
+      searchMovie: "",
+    };
+  },
+  watch: {
+    searchMovie: debounce(function (newVal) {
+      this.$store.dispatch("movies/search_movie", newVal);
+      this.$router.push({ name: "Search", params: { name: newVal } });
+    }, 500),
+  },
+};
 </script>
 
 <style scoped>
-
 .search-box {
   display: block;
   width: 100%;
@@ -37,16 +37,15 @@ import debounce from "lodash/debounce";
   appearance: none;
   outline: none;
   background: none;
-  background-color:#fff;
+  background-color: #fff;
   border-radius: 0px 16px 0px 16px;
   transition: 0.4s;
-  border-radius:25px;
-  
+  border-radius: 25px;
 }
 .search-box:focus {
   box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
-  background-color:#fff;
+  background-color: #fff;
   border-radius: 16px 0px 16px 0px;
-  border:1px solid black;
+  border: 1px solid black;
 }
 </style>
