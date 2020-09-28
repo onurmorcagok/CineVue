a877e704ddf175337c0ee86528973ae52880ab03
Onur MORCAGÖK <55251990+onurmorcagok@users.noreply.github.com>
2020-09-28 15:28:03 +0300

    Project updated

--- cinevue/src/api/api.js
+++ cinevue/src/api/api.js
@@ -1,6 +1,6 @@
 export const API = {
     API_KEY: "c038ce1188345d8eaab23ae93ef8532d",
-    IMAGE_URL: "http://image.tmdb.org/t/p/original/",
+    IMAGE_URL: "https://image.tmdb.org/t/p/original/",
     TRAILER_URL: "https://www.youtube.com/embed/",
     MOVIE: "movie/",
     SEARCH: "search/",
