c3beda9e9fb2ef27c899b0330c09fc330dba7e79
Onur MORCAGÖK <55251990+onurmorcagok@users.noreply.github.com>
2020-09-28 17:10:59 +0300

    Project updated

--- cinevue/src/components/Footer.vue
+++ cinevue/src/components/Footer.vue
@@ -1,17 +1,21 @@
 <template>
-<nav class="navbar text-dark bg-warning">
+  <nav class="navbar text-dark bg-warning">
     <span class="navbar-text mr-auto text-dark">
-        <strong>©2020 All Rights Reserved</strong>
-       </span>
-          <!--Linkedin -->
-          <a class="li-ic" href="https://www.linkedin.com/in/onurmorcagok/" target="_blank">
-            <i class="fab fa-linkedin-in fa-lg text-dark mr-md-5 mr-3"> </i>
-          </a>
-          <!--GitHub-->
-          <a class="ins-ic" href="https://github.com/onurmorcagok" target="_blank">
-            <i class="fab fa-github fa-lg text-dark mr-md-5 mr-3"> </i>
-          </a>    
-</nav>
+      <strong>©2020 All Rights Reserved</strong>
+    </span>
+    <!--Linkedin -->
+    <a
+      class="li-ic"
+      href="https://www.linkedin.com/in/onurmorcagok/"
+      target="_blank"
+    >
+      <i class="fab fa-linkedin-in fa-lg text-dark mr-md-5 mr-3"> </i>
+    </a>
+    <!--GitHub-->
+    <a class="ins-ic" href="https://github.com/onurmorcagok" target="_blank">
+      <i class="fab fa-github fa-lg text-dark mr-md-5 mr-3"> </i>
+    </a>
+  </nav>
 </template>
 
 <script>
@@ -21,5 +25,4 @@ export default {
 </script>
 
 <style scoped>
-
 </style>
