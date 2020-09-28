fdb7dcfb23ef749f271f86f4c240af0123b34588
Onur MORCAGÖK <55251990+onurmorcagok@users.noreply.github.com>
2020-09-28 15:42:12 +0300

    Project updated

--- cinevue/src/api/api.js
+++ cinevue/src/api/api.js
@@ -1,7 +1,7 @@
 export const API = {
     API_KEY: "c038ce1188345d8eaab23ae93ef8532d",
-    IMAGE_URL: "https://image.tmdb.org/t/p/original/",
-    TRAILER_URL: "https://www.youtube.com/embed/",
+    IMAGE_URL: "http://image.tmdb.org/t/p/original/",
+    TRAILER_URL: "http://www.youtube.com/embed/",
     MOVIE: "movie/",
     SEARCH: "search/",
     CREDITS: "/credits",
--- cinevue/src/utils/axios.js
+++ cinevue/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const httpService = axios.create({
-  baseURL: "https://api.themoviedb.org/3/",
+  baseURL: "http://api.themoviedb.org/3/",
 });
 
 httpService.interceptors.response.use(
