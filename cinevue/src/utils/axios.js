a5f9b31f4f7c08ad0aa0548c30697fe836b36a8b
Onur MORCAGÖK <55251990+onurmorcagok@users.noreply.github.com>
2020-09-28 15:46:32 +0300

    Project updated

--- cinevue/src/utils/axios.js
+++ cinevue/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const httpService = axios.create({
-  baseURL: "http://api.themoviedb.org/3/",
+  baseURL: "https://api.themoviedb.org/3/",
 });
 
 httpService.interceptors.response.use(
