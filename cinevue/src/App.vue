a8a0bdfb45afcac8be3601b72ec0f342d9e2ef51
Onur MORCAGÖK <55251990+onurmorcagok@users.noreply.github.com>
2020-09-29 19:42:41 +0300

    Update App.vue

--- cinevue/src/App.vue
+++ cinevue/src/App.vue
@@ -2,9 +2,9 @@
   <div id="app">
     <appNavbar></appNavbar>
     <div class="mx-auto">
-    <transition name="slide" mode="out-in">
-      <router-view></router-view>
-    </transition>
+      <transition name="slide" mode="out-in">
+        <router-view></router-view>
+      </transition>
     </div>
     <appFooter></appFooter>
   </div>
